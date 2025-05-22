be21c0496ebf92786f7ae8763938a0e18ab6c8a5
Shandeepsugumar <112404974+Shandeepsugumar@users.noreply.github.com>
2025-05-22 22:09:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,9 @@ app.post('/api/logout', (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
